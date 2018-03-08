6320f7c253cf6d3544079453920cd5cb5fbc72e5
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-08 23:03:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function add(a, b) {
   return a + b
 }
-function 
+function wubtract(a, b) {
+  return a - b
+}
+function multiply(a, b) {
+  return a * b
+}
+function divide(a, b) {
+  return a / b
+}
